ad5e528a40561c7d5272521b8cc7480b9b79a086
ST10083235-Balraj <102229020+ravelleb@users.noreply.github.com>
2024-04-02 19:15:38 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using cldv_part1_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace cldv_part1_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Work()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Views/Home/Work.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cldv_part1_mvc.Views.Home
+{
+    public class WorkModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
